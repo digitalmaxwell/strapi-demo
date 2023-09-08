d2a3c0e7a444676d42d38c4cc85cf674c66f9afc
Maxwell Chandler <personal@digitalmaxwell.com>
2023-09-08 13:42:42 -0400

    default strapi backend starter + nextjs ts/eslint starter. Setup default folders for strapi helper funcs

---
+++ frontend/strapi-demo/pages/default-lander/[[...id]].tsx
@@ -0,0 +1,6 @@
+import strapiFetchGraphQL from '/src/lib/strapi/fetchGraphQL';
+import strapiGetStaticPaths from '/src/lib/strapi/getStaticPaths';
+import strapiGetCollection from '/src/lib/strapi/getCollection';
+import strapiGetIdBySlug from '/src/lib/strapi/getIdBySlug';
+import strapiComponentListIsValid from '/src/lib/strapi/componentListIsValid';
+
